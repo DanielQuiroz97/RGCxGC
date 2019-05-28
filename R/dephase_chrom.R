89e5c1279d80b6a2e982aae1358ac1f6850808e6
DanielQuiroz97 <cristianquirozd1997@gmail.com>
2019-05-28 09:01:48 -0500

    Add dephase_chrom fun

---
+++ R/dephase_chrom.R
@@ -0,0 +1,42 @@
+#' @export
+#' @docType methods
+#' @rdname dephase_chrom-methods
+setGeneric(name = "dephase_chrom",
+           def = function(Object, rel_dephase)
+             standardGeneric("dephase_chrom"))
+
+#' @title  Method dephase_chrom
+#' @rdname dephase_chrom-methods
+#' @aliases dephase_chrom,GCxGC-method
+#' @description `dephase_chrom`  move the retention time in the second dimension of a
+#'  bidimensional chromatogram. This procedure is ussually applied in cases
+#'  when peaks are near at the final of the second dimension of the 
+#'  chromatogram. The dephasing is performing by splitting the chromagram
+#'  with the relative value provided.
+#'  
+#'  @param Object a GCxGC class object
+#'  @param rel_dephase a numeric value from 0 to 100 with the relative dephasing
+#'    position.
+#'  @export
+#'  @examples 
+#'  
+#'  GB08_fl <- system.file("extdata", "08GB.cdf", package = "RGCxGC")
+#'  GB08 <- read_chrom(GB08_fl, 5L)
+#'  plot(GB08, nlevels = 150, color.palette = matlab.like,
+#'       main = "No dephased chromatogram")
+#'  GB08_d25 <- dephase_chrom(GB08, 25)
+#'  plot(GB08_d25, nlevels = 150, color.palette = matlab.like,
+#'       main = "25% dephased chromatogram")
+setMethod(f = "dephase_chrom",
+          signature = c("GCxGC"),
+          definition =  function(Object, rel_dephase) {
+            if (rel_dephase < 0  | rel_dephase > 100)
+              stop("A relative value from 0 to 100 must be provided")
+            chrom_rows <- nrow(Object@chromatogram)
+            dephase_index <- seq(1, chrom_rows * rel_dephase / 100)
+            chrom1 <- Object@chromatogram[dephase_index, ]
+            chrom2 <- Object@chromatogram[-dephase_index, ]
+            chrom <- rbind(chrom2, chrom1)
+            Object@chromatogram <- chrom
+            return(Object)
+          })
