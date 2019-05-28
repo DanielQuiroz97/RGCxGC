97c04a52e6141e11e3e14950b4ebd1b3ed5ffa4f
DanielQuiroz97 <cristianquirozd1997@gmail.com>
2019-05-28 08:31:43 -0500

    Add dephase_chrom fun

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,7 @@
+
+setGeneric(name = "get_metadata",
+           def = function(Object)
+             standardGeneric("get_metadata"))
 #' Extract metadata from a joinded_chrom
 #' 
 #' `get_metadata` if a metadata was provided when join_chromatogram
@@ -14,15 +18,15 @@
 #'                        Type = c("Control", "Treatment"))
 #' join_metadata <- join_chromatograms(GB08, GB09, groups = metadata)
 #' get_metadata(join_metadata)
-setGeneric(name = "get_metadata",
-           def = function(Object)
-             standardGeneric("get_metadata"))
-
 setMethod(f = "get_metadata",
           signature = "joined_chrom",
           definition = function(Object){
             return(Object@groups)
           })
+
+setGeneric(name = "set_metadata",
+           def = function(Object, metadata)
+             standardGeneric("set_metadata"))
 
 #' Set the metadata for a joined_chrom
 #' 
@@ -42,16 +46,17 @@ setMethod(f = "get_metadata",
 #' join_chrom <- join_chromatograms(GB08, GB09)
 #' join_metadata <- set_metadata(join_chrom, metadata = extra_info)
 
-setGeneric(name = "set_metadata",
-           def = function(Object, metadata)
-             standardGeneric("set_metadata"))
-
 setMethod(f = 'set_metadata',
           signature = c("joined_chrom", "data.frame"),
           definition = function(Object, metadata){
             Object@groups <- metadata
             return(Object)
           })
+
+
+setGeneric(name = "dephase_chrom",
+           def = function(Object, rel_dephase)
+             standardGeneric("dephase_chrom"))
 
 #' Dephase the 2D retention time
 #' 
@@ -74,13 +79,6 @@ setMethod(f = 'set_metadata',
 #'  GB08_d25 <- dephase_chrom(GB08, 25)
 #'  plot(GB08_d25, nlevels = 150, color.palette = matlab.like,
 #'       main = "25% dephased chromatogram")
-
-
-setGeneric(name = "dephase_chrom",
-           def = function(Object, rel_dephase)
-             standardGeneric("dephase_chrom"))
-
-
 setMethod(f = "dephase_chrom",
           signature = c("GCxGC"),
           definition =  function(Object, rel_dephase) {
@@ -94,6 +92,3 @@ setMethod(f = "dephase_chrom",
             Object@chromatogram <- chrom
             return(Object)
           })
-
-
-
