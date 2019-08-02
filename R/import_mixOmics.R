f20408f4958cb19c0f347f1a66221ebcd094094a
DanielQuiroz97 <cristianquirozd1997@gmail.com>
2019-08-02 15:39:34 -0500

    minor changes

---
+++ R/import_mixOmics.R
@@ -0,0 +1,66 @@
+#' @export
+#' @docType methods
+#' @rdname import_mixOmics-methods
+setGeneric(name = "import_mixOmics",
+           def = function(chromatogram, model, mod_time, 
+                          time_range, sampling_rt){
+             standardGeneric("import_mixOmics")
+           })
+
+#' @title Import mixOmics discriminant analysis
+#' @rdname import_mixOmics-methods
+#' @aliases import_mixOmics,PLSDA-method
+#' @description `import_mixOmics` transform a mixOmics discriminant analysis
+#'   lodings/eigenvectors into a two-dimensional loading matrix
+#' @details This functions takes a model built through
+#'   \code{\link[mixOmics]{plsda}} and \code{\link[mixOmics]{splsda}}, then,
+#'   access to loading values and transform each dimension loading into
+#'   a two-dimensional matrix. This matrix represents a typcial GCxGC
+#'   chromatogram. By default, user can provide a chromatogram where the
+#'   requiered information will be retrieved from. On the other hand,
+#'   user can also provide all the needed information
+#'   (\emph{mod_time}, \emph{time_range}, \emph{sampling_rt})
+#'   to fold eigenvectors into a GCxGC chromatogram.
+#'   
+#' @param chromatogram a typicial GCxGC imported or preprocessed chromatogram.
+#' @param model a partial least square discriminant analysis based model, built
+#'   by mixOmics package.
+#' @param mod_time modulation time of the second dimension
+#' @param time_range an atomic vector of two with the time range
+#'   of chromatographic run
+#' @param sampling_rate the sampling rate of the equipment
+#' @exportMethod import_mixOmics
+setMethod(f = "import_mixOmics", signature = "GCxGC",
+          definition = function(chromatogram, model, mod_time,
+                                time_range, sampling_rt) {
+            
+            if (!missing(chromatogram)) {
+              if ( !(is(model, "mixo_splsda") | is(model, "mixo_plsda")) )
+                stop("Please, provide a model builded by plsda or 
+                splda function by mixOmics package")
+              raw_loadings <- as.data.frame(model$loadings$X)
+              chrom <- chromatogram@chromatogram
+              n_row <- nrow(chrom)
+              n_col <- ncol(chrom)
+              loadings_2d <- lapply(raw_loadings,
+                                    FUN = function(lds, n_row, n_col){
+                matrix(lds, ncol = n_col, nrow = n_row, byrow = T)
+              }, n_row = n_row, n_col = n_col  )
+              
+            } else{
+              raw_loadings <- as.data.frame(model$loadings$X)
+              len_1d <- floor(sam_rate * mod_time)
+              len_2d <- floor(ncol(raw_loadings) / len_1d)
+              loadings_2d <- lapply(raw_loadings,
+                                    FUN = function(lds, n_row, n_col){
+                matrix(lds,nrow = len_1d, ncol = len_2d, byrow = T)
+              }  )
+            }
+            mod_time <- chromatogram@mod_time
+            time <- chromatogram@mod_time
+            foreign_model <- new("PLSDA", loadings = loadings_2d,
+                                 mod_time = mod_time, time = time)
+            foreign_model
+            
+          })
+
